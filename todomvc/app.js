a2e145051f289b902b54981c310e14abab9c809d
xinchou16 <zhouxinmine@live.com>
2018-04-23 20:37:29 +0800

    add filter

--- todomvc/app.js
+++ todomvc/app.js
@@ -5,11 +5,19 @@
  Vue.component('todo-list',{
    template:`
    <li class="list">
-      <p>{{ content.text}}</p>
-      <button class="del" 
-      v-on:click="delTodo(index)">
-      删除
-      </button>
+    <p>
+      <input type="checkbox" 
+        v-model="content.done"
+        v-on:click="finishTodo(content)"
+       >
+      <span class="todo-text"
+        :class="{finished:content.done}"
+      >{{ content.text}}</span>
+    </p>
+    <button class="del" 
+    v-on:click="delTodo(index)">
+    删除
+    </button>
     </li>
    `,
    props:['content','index'],
@@ -17,9 +25,18 @@
     delTodo: function (index) {
       // camelCase is not allowed
       this.$emit('delete',this.index)
+    },
+    finishTodo: function (content) {
+      // console.log(content.text+' is '+ content.done);
     }
+   },
+   data: function() {
+     return {
+       "isChecked": false,
+     }
    }
  })
+
 
 var vm = new Vue({
   data: {
@@ -28,17 +45,37 @@ var vm = new Vue({
     parentMsg:'parentMsg'
   },
   methods: {
+    // add todo 
     addTodo: function () {
+      var value = this.todoText && this.todoText.trim()
+      if(!value) {return}
+
       this.todos.push({
-        text: this.todoText,
+        id: 1,
+        text: value,
         done: false,
       })
     },
 
-    // delete
+    // delete todo
     delTodo: function(index) {
       var item = this.todos.splice(index,1)
-      console.log('你删除了第  ' +(index+1) +'项');
+      console.log('你删除了 ' +item[0].text );
+    },
+
+  },
+
+  // filters
+  filters: {
+    
+  },
+
+  // custom directives
+  directives: {
+    focus: {
+      inserted: function(el) {
+        el.focus(); // 自动聚焦到输入框
+      }
     }
   }
 }).$mount('#app')
